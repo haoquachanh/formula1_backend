36b34cc4483060f4744d441346c6edd493ea58f3
haoquachanh <anhhaomnmm@gmail.com>
2023-06-16 22:22:39 +0700

    update config

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,22 @@
 import "reflect-metadata";
 import express from "express";
 import { createConnection } from "typeorm";
-// import userRoutes from "./api/routes/userRoutes";
+import routes from "./api/routes";
+import 'dotenv/config'
 
 const app = express();
-const port = 3000;
-
-app.use(express.json());
-// app.use("/users", userRoutes);
-
-// console.log(port);
-
+const port = process.env.PORT || 3001;
 createConnection()
   .then(() => {
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   })
-  .catch((error) => console.log(error));
+  .catch((error) => console.log('Failed in connecting database: ',error));
+
+app.use(express.json());
+
+// Thêm các cấu hình
+// app.use('/api/v1',routes)
+app.use('/',routes)
+
